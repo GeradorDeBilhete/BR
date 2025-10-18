903efec8917ae9d7b917490928850904a17a340e
BeBelBet <kawansilva7771@gmail.com>
2025-10-18 14:53:12 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,20 @@
+const cacheName = "bilhete-cache-v1";
+const assets = [
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png",
+  "https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => cache.addAll(assets))
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
+  );
+});
