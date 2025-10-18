209dfb1ed0f4c2fc704d1c8773702156a368c870
GeradorDePalpite <kawansilva7771@gmail.com>
2025-10-18 17:19:12 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,2 @@
-const cacheName = "bilhete-cache-v1";
-const assets = [
-  "./index.html",
-  "./manifest.json",
-  "./icon-192.png",
-  "./icon-512.png",
-  "https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js"
-];
-
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open(cacheName).then(cache => cache.addAll(assets))
-  );
-});
-
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(response => response || fetch(e.request))
-  );
-});
+self.addEventListener('install', e => console.log('Service Worker instalado'));
+self.addEventListener('fetch', e => {});
